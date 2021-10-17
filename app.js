56c47156e0ae0e4c0d164b0a2b242bf05deb6291
andy2148deathedge <andy2148deathedge@gmail.com>
2021-10-17 15:48:04 +0800

    feat: add creat, edit feature and page

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // import library
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 // const mongoose = require('mongoose');
 
 // import self made library
@@ -17,6 +19,8 @@ const PORT = 3000;
 // setting & middleware
 app.engine('hbs', exphbs({ defaultLayout: "main", extname: "hbs" }));
 app.set('view engine', 'hbs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 
 // routing
@@ -45,9 +49,35 @@ app.get('/new', (req, res) => {
 
 app.post('/record', (req, res) => {
   // 拿到 new 表單內容存入資料庫 並且重定向至 '/'
-  res.send('new表單送出了');
+  let newRecord = req.body;
+  newRecord.amount = Number(newRecord.amount);
+  Record.create(newRecord);
+  
+  res.redirect('/');
 });
 
+app.get('/record/:_id/edit', (req, res) => {
+  const id = req.params._id;
+
+  return Category.find()
+    .lean()
+    .sort({ _id: 'asc'})
+    .then((categories) => Record.findById(id)
+      .lean()
+      .then(record => res.render('edit', {categories, record}))
+      .catch(e => console.log(e)) )
+    .catch(e => console.log(e))
+});
+
+app.put('/record/:id', (req, res) => {
+  const id = req.params.id;
+  let newRecord = req.body;
+  newRecord.amount = Number(newRecord.amount);
+  
+  return Record.updateOne({ _id: id }, newRecord)
+    .then(() => res.redirect('/'))
+    .catch(e => console.log(e));
+});
 
 // Server listen
 app.listen(PORT, () => {
