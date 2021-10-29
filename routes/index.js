97b1cc6469bcd89c0ce183ef44213c5cd72e6b24
andy2148deathedge <andy2148deathedge@gmail.com>
2021-10-29 13:26:11 +0800

    feat: setting 'users' router

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const home = require('./modules/home');
-router.use('/', home);
-
 const record = require('./modules/record');
+const users = require('./modules/users');
+
+router.use('/', home);
 router.use('/record', record); 
+router.use('/users', users);
 
 module.exports = router;
