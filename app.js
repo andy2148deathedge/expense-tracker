9c57f87cb7d0437fc628fad0fe10d83c82145a7f
andy2148deathedge <andy2148deathedge@gmail.com>
2021-10-29 16:54:43 +0800

    feat: res.local isAuthenticated to show login/logout state in main layout

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ app.use(methodOverride('_method'));
 
 // routing
 usePassport(app);
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated();
+  res.locals.user = req.user;
+  next();
+});
 app.use(routes);
 
 // Server listen
