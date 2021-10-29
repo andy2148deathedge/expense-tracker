29db07acacd306cadccc6030ec5800058c0532b3
andy2148deathedge <andy2148deathedge@gmail.com>
2021-10-29 15:03:22 +0800

    feat: setting express-session

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import library
 const express = require('express');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
@@ -19,6 +20,13 @@ const helpers = hbsHelpers();
 // setting & middleware
 app.engine('hbs', exphbs({ defaultLayout: "main", extname: "hbs", helpers: helpers}));
 app.set('view engine', 'hbs');
+
+app.use(session({
+  secret: 'ThisisMySecret',
+  resave: false,
+  saveUninitialized: true
+}));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
