3db1d275e4cb4b87122a5233eef2a21697d71bfa
andy2148deathedge <andy2148deathedge@gmail.com>
2021-10-29 18:55:00 +0800

    feat: import bcryptjs & hash the password of registration

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const hbsHelpers = require('handlebars-helpers');
+const flash = require('connect-flash');
 
 // import self made library
 const routes = require('./routes');
@@ -20,21 +21,22 @@ const helpers = hbsHelpers();
 // setting & middleware
 app.engine('hbs', exphbs({ defaultLayout: "main", extname: "hbs", helpers: helpers}));
 app.set('view engine', 'hbs');
-
 app.use(session({
   secret: 'ThisisMySecret',
   resave: false,
   saveUninitialized: true
 }));
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 // routing
 usePassport(app);
+app.use(flash());
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated();
   res.locals.user = req.user;
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.warning_msg = req.flash('warning_msg');
   next();
 });
 app.use(routes);
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const bcrypt = require('bcryptjs');
 
 const User = require('../../models/user');
 
@@ -21,15 +22,31 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const {name, email, password, confirmPassword} = req.body;
+  const errors = [];
+
+  if (!name || !email || !password || !confirmPassword) {
+    errors.push({ message: '還有欄位沒填寫哦!' });
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼 & 確認密碼欄位不相符!'})
+  }
+  if (errors.length) {
+    return res.render('register', { errors, name, email, password, confirmPassword })
+  }
 
   User.findOne({email})
     .then(user => {
       if (user) {
-        console.log('此信箱已被註冊。');
-        return res.render('register', {name, email, password, confirmPassword});
+        errors.push({ message: '此信箱已被註冊。'});
+        return res.render('register', {errors, name, email, password, confirmPassword});
       }
 
-      return User.create({name, email, password, confirmPassword})
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => 
+          User.create({name, email, password: hash})
+        )
         .then(() => res.redirect('/'))
         .catch(e => console.log(e));
     })
@@ -38,6 +55,7 @@ router.post('/register', (req, res) => {
 
 router.get('/logout', (req, res) => {
   req.logout();
+  req.flash('success_msg', '成功登出!');
   res.redirect('/users/login');
 });
 
